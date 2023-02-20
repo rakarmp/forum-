55a28be57c04f536d21bb2af6b60165ae622a232
grizz12 <raka_088@smkassalaambandung.sch.id>
2023-02-19 22:46:13 -0800

    fixing

--- app/Http/Controllers/Pages/HomeController.php
+++ app/Http/Controllers/Pages/HomeController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use App\Policies\UserPolicy;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Thread;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
